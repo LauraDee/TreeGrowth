ad2ae58f948df012c4310b2522923ef0d939cb0e
Laura E Dee <ledee@nat-10-22-29-13.uofm.wireless.umn.edu>
2017-01-10 12:52:57 -0600

    merged prism data and MM tree FIA data

--- code/Merging PRISM subset and FIA.R
+++ code/Merging PRISM subset and FIA.R
@@ -3,11 +3,12 @@
 # Laura Dee - Jan 10 2017 
 
 require(data.table)
+reqiuire()
 setwd("~/Google Drive/Tree growth drivers/data")
 
 ### Read in Data ######
-# matched PRISM and FIA data for a subset of sites (with lat long) 
-## ??? determine what subset Grant used. paper reference?
+  # matched PRISM and FIA data for a subset of sites (with lat long) 
+  # ??? determine what subset Grant used. paper reference?
   for_prism_dat <- read.csv("LS_data_081016.csv")
 
   ## Load the FIA data for MN
@@ -19,6 +20,9 @@ setwd("~/Google Drive/Tree growth drivers/data")
   ## all species in FIA matching FIA species code to taxonomy and common name, which region etc
   speciescodes <- read.csv("REF_SPECIES_FIAcodes.csv")
   
+### merge datasets by plot and inventory year ###
+  climate_tree_MNdat <- merge(for_prism_dat, treespdat, by = c("PLT_CN", "INVYR"), all = FALSE)
   
-
+## plot at relative growth by year ## 
+  plot(GROWCFGS ~ INVYR, data = climate_tree_MNdat)
   
