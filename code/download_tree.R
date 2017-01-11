0b50dcc074da956a3f17a338c35abbc325a2733c
Laura E Dee <ledee@Unknown.domain>
2017-01-11 08:27:06 -0600

    added annotation

--- code/download_tree.R
+++ code/download_tree.R
@@ -1,4 +1,9 @@
-download_tree <- function(state){
+#### Downloading the FIA data, the "TREE" files by state from 
+# https://apps.fs.usda.gov/fiadb-downloads/CSV/datamart_csv.html
+## Jan 2017 
+
+
+## create a function to download the _TREE.csv files for each state download_tree <- function(state){
   path <- "data"
   dir.create(path, FALSE)
   state_csv <- paste0(state,"_TREE.csv")
@@ -7,10 +12,12 @@ download_tree <- function(state){
   downloader::download(dwld, destfile= destfile)
 }
 
+#get list of state abbreviations from the FIA data 
 setwd("~/Google Drive/Tree growth drivers/data/")
 statesFIA <- read.csv("data/REF_FIAstatecodes.csv")
 state_list <- as.character(unique(statesFIA$STATE_ABBR))
-# state_list <- c("CT","MA")
+# or do manually or for a subset of states:
+#state_list <- c("CT","MA")
 
 #Download state level tree data
 lapply(state_list[c(7,22)], download_tree)
